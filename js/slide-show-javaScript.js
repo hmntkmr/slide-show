2c5fa5232f463f2ab9ec22fca00eca36997e2647
Hemant Kumar <hemantkumar@Hemants-MacBook-Pro.local>
2016-05-26 00:45:47 -0400

    up and running
    All files and slide show running

---
+++ js/slide-show-javaScript.js
@@ -0,0 +1,61 @@
+
+
+            var images = ["<figure><img src=\"images/us-trip-1.jpg\" alt=\"An awesome picture1\" class=\"img-responsive center-block img-rounded\"><br><br><figcaption>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</figcaption></figure>",
+            "<figure><img src=\"images/us-trip-2.jpg\" alt=\"An awesome picture1\" class=\"img-responsive center-block img-rounded\"><br><figcaption>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</figcaption></figure>",
+            "<figure><img src=\"images/us-trip-3-hor.jpg\" alt=\"An awesome picture1\" class=\"img-responsive center-block img-rounded\"><br><figcaption>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</figcaption></figure>",
+            "<figure><img src=\"images/us-trip-4-hor.jpg\" alt=\"An awesome picture1\" class=\"img-responsive center-block img-rounded\"><br><figcaption>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</figcaption></figure>",
+            "<figure><img src=\"images/us-trip-5.jpg\" alt=\"An awesome picture1\" class=\"img-responsive center-block img-rounded\"><br><figcaption>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</figcaption></figure>",
+            "<figure><img src=\"images/us-trip-6.jpg\" alt=\"An awesome picture1\" class=\"img-responsive center-block img-rounded\"><br><figcaption>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</figcaption></figure>",
+            "<figure><img src=\"images/us-trip-7.jpg\" alt=\"An awesome picture1\" class=\"img-responsive center-block img-rounded\"><br><figcaption>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</figcaption></figure>",
+            "<figure><img src=\"images/us-trip-8.jpg\" alt=\"An awesome picture1\" class=\"img-responsive center-block img-rounded\"><br><figcaption>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</figcaption></figure>",
+            "<figure><img src=\"images/us-trip-9.jpg\" alt=\"An awesome picture1\" class=\"img-responsive center-block img-rounded\"><br><figcaption>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</figcaption></figure>",
+            "<figure><img src=\"images/us-trip-10.jpg\" alt=\"An awesome picture1\" class=\"img-responsive center-block img-rounded\"><br><figcaption>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</figcaption></figure>",
+            "<figure><img src=\"images/us-trip-11.jpg\" alt=\"An awesome picture1\" class=\"img-responsive center-block img-rounded\"><br><figcaption>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</figcaption></figure>",
+            "<figure><img src=\"images/us-trip-12.jpg\" alt=\"An awesome picture1\" class=\"img-responsive center-block img-rounded\"><br><figcaption>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</figcaption></figure>",
+            "<figure><img src=\"images/us-trip-13.jpg\" alt=\"An awesome picture1\" class=\"img-responsive center-block img-rounded\"><br><figcaption>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</figcaption></figure>",
+            "<figure><img src=\"images/us-trip-14.jpg\" alt=\"An awesome picture1\" class=\"img-responsive center-block img-rounded\"><br><figcaption>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</figcaption></figure>",
+            "<figure><img src=\"images/us-trip-15.jpg\" alt=\"An awesome picture1\" class=\"img-responsive center-block img-rounded\"><br><figcaption>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</figcaption></figure>"];
+
+            // arrary of images
+
+
+            var imageContainer = document.getElementById("slideShow"); //variable to getElementById 
+
+            var i=0; 
+
+            setInterval(imageLoader, 5000); //inbuilt function setInterval for loading image
+
+
+            function imageLoader(){         //function declared and defined to load images after 4 seconds
+
+
+            if (i > images.length - 1){         //if statement to check the lenght of the array
+                
+                i=0;
+
+            }
+
+
+          
+            imageContainer.style.opacity = 1;  //changes opacity to 1
+
+            imageContainer.innerHTML = images[i]; // insert elements of an array into a div using innerHTML property
+
+            setTimeout(fadeOut, 4000);          // using inbuilt function setTimeout for changing the image
+
+            i+=1;       // increasing the pointer of an array to keep looping the images
+
+            }               // image loader function closed
+
+
+            imageLoader();  // giving call to function image loader
+
+
+
+            function fadeOut(){  //function declared and defined fadeout
+            
+            imageContainer.style.opacity = 0; //setting opacity of the image container to zero;
+            
+            }
+
+</script>
